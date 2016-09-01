8f22333868c792d4120347893dbc69133e707937
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-09-01 16:22:31 -0300

    Update tests target with viewModel changes

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPagoContext.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoContext.swift
@@ -291,8 +291,7 @@ public class MercadoPagoContext : NSObject, MPTrackerDelegate {
     }
     
     public class func isCustomerInfoAvailable() -> Bool {
-        //TODO 
-        return false//(self.sharedInstance.base_url.characters.count > 0 && self.sharedInstance.customer_uri.characters.count > 0 && self.sharedInstance.merchant_access_token.characters.count > 0)
+        return (self.sharedInstance.base_url.characters.count > 0 && self.sharedInstance.customer_uri.characters.count > 0 && self.sharedInstance.merchant_access_token.characters.count > 0)
     }
     
     public class func paymentKey() -> String {
